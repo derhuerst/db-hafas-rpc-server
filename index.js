dad99a7fdcb69d297b0210aff070d19577ad9b8e
Jannis R <mail@jannisr.de>
2018-12-05 13:41:07 +0100

    implementation :sparkles:

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 'use strict'
 
-// todo
+const http = require('http')
+const createHafas = require('db-hafas')
+const exposeHafasClient = require('hafas-client-rpc/server')
+
+const port = process.env.PORT || 3000
+
+const httpServer = http.createServer()
+const hafas = createHafas('db-hafas-rpc-server')
+const server = exposeHafasClient(httpServer, hafas)
+
+httpServer.listen(port, (err) => {
+	if (err) {
+		console.error(err)
+		process.exitCode = 1
+	} else console.info(`Listening on ${port}.`)
+})
